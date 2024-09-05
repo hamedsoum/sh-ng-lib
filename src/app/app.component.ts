16558e26d25d4d57450ee661df21de49a7cccdf0
hamedsoum <hamedsoum4023@gmail.com>
2024-09-05 00:01:06 +0000

    app created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -10,4 +10,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'sh-ng-lib';
+
+
+   count = signal(0);
 }
