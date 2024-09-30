01cd3730f181da1e3abbe2b33866e392d04a2520
Hamed Soumahoro <hamed.soumahoro@iro-xs.com>
2024-09-30 09:11:39 +0000

    function create in auth base service

--- projects/sh-core/src/lib/authentication/sh-authentication-base.service.ts
+++ projects/sh-core/src/lib/authentication/sh-authentication-base.service.ts
@@ -3,4 +3,12 @@ export abstract class SHAuthenticationBaseService {
   public login(): void {
     console.log("SH ng lib Authentication base service login method")
   }
+
+  public isAuthenticate(): boolean {
+    return false;
+  }
+
+  public registration(): void {
+
+  }
 }
